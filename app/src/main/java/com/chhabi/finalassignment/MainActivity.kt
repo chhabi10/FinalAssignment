492dc189a323e61b8d32e9281cc0c04129661c4b
chhabi bista <bistachhabi29@gmail.com>
2021-01-22 16:42:06 +0545

    Homepage of app

--- app/src/main/java/com/chhabi/finalassignment/MainActivity.kt
+++ app/src/main/java/com/chhabi/finalassignment/MainActivity.kt
@@ -2,18 +2,31 @@ package com.chhabi.finalassignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
 
 
-
+private lateinit var imgv:ImageView
+    private lateinit var btnenter: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        imgv=findViewById(R.id.imgv)
+        btnenter= findViewById(R.id.btnenter)
+
+        btnenter.setOnClickListener(this)
 
 
+
+    }
+
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
     }
 }
