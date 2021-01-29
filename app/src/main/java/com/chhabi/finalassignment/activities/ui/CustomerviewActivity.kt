2b84b60b1d382aa0ee90ad82c3721d8d4e6249b8
chhabi bista <bistachhabi29@gmail.com>
2021-01-29 20:25:33 +0545

    dashboard

---
+++ app/src/main/java/com/chhabi/finalassignment/activities/ui/CustomerviewActivity.kt
@@ -0,0 +1,12 @@
+package com.chhabi.finalassignment.activities.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.chhabi.finalassignment.R
+
+class CustomerviewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customerview)
+    }
+}
--- app/src/main/java/com/chhabi/finalassignment/activities/ui/DashboardActivity.kt
+++ app/src/main/java/com/chhabi/finalassignment/activities/ui/DashboardActivity.kt
@@ -1,12 +1,20 @@
 package com.chhabi.finalassignment.activities.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.chhabi.finalassignment.R
 
 class DashboardActivity : AppCompatActivity() {
+private lateinit var btnView:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        btnView=findViewById(R.id.btnView)
+        btnView.setOnClickListener {
+            startActivity(Intent(this, CustomerviewActivity::class.java))
+        }
     }
 }
