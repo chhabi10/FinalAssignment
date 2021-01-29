ed14b821001f9fba1f73e0ac69d41d14b069d9ae
chhabi bista <bistachhabi29@gmail.com>
2021-01-29 19:05:41 +0545

    SplashscreenActivitychanges

--- app/src/main/java/com/chhabi/finalassignment/activities/ui/SplashscreenActivity.kt
+++ app/src/main/java/com/chhabi/finalassignment/activities/ui/SplashscreenActivity.kt
@@ -3,6 +3,8 @@ package com.chhabi.finalassignment.activities.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import com.chhabi.finalassignment.R
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -10,14 +12,20 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class SplashscreenActivity : AppCompatActivity() {
+
+    private lateinit var img: ImageView
+    private lateinit var txtv:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
 
+        img= findViewById(R.id.img)
+        txtv=findViewById(R.id.txtv)
+
 
         CoroutineScope(Dispatchers.Main).launch {
             delay(1000)
-            startActivity(Intent(this@SplashscreenActivity, MainActivity::class.java))
+            startActivity(Intent(this@SplashscreenActivity,LoginActivity::class.java))
             finish()
         }
 
