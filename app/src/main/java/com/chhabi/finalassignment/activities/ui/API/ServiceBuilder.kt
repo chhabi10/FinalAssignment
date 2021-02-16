2009311f1debcbd537143ddc2ea2e5462e24c14c
chhabi bista <bistachhabi29@gmail.com>
2021-02-16 21:05:33 +0545

    Api request

---
+++ app/src/main/java/com/chhabi/finalassignment/activities/ui/API/ServiceBuilder.kt
@@ -0,0 +1,4 @@
+package com.chhabi.finalassignment.activities.ui.API
+
+class ServiceBuilder {
+}
---
+++ app/src/main/java/com/chhabi/finalassignment/activities/ui/Response/LoginResponse.kt
@@ -0,0 +1,4 @@
+package com.chhabi.finalassignment.activities.ui.Response
+
+class LoginResponse {
+}
